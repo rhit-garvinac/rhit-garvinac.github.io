7c2a6f91e8a93330d234f290946ed22fa038f3ed
Soupman <arthur.campbell.garvin@gmail.com>
2021-09-28 14:19:37 -0400

    initial

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+function scrollFunction() {
+    btn = document.getElementById("scrollButton");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function main(){
+    btn = document.getElementById("scrollButton");
+    btn = (event) => {
+        topFunction()
+        };
+    window.onscroll = function() {scrollFunction()};
+}
+
+main();
